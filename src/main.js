bc2d9088619e5eb56dc97b1b16aa92122665989b
Aylin <aylin_santacruz@hotmail.com>
2024-01-05 11:59:32 -0500

    chore(main.js): rendered in HTML

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { example } from './dataFunctions.js';
+import { filterData,sortData } from './dataFunctions.js';
 import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
 
-console.log(example, renderItems(data), data);
+// console.log(example, renderItems(data), data);
+
+const containerCard = document.querySelector("#root");
+
+containerCard.innerHTML = renderItems(data);
