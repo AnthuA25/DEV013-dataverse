9ff11692d25b23c80e8b3ce89645d3190039d424
Aylin <aylin_santacruz@hotmail.com>
2024-01-06 16:47:57 -0500

    feat(dataFunctions.js): add function filterData

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,9 +1,9 @@
 // Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
 
 export const filterData = (data, filterBy,value) => {
-  return 'example';
+  const result = data.filter(type => type.type[filterBy].includes(value));
+  return result;
 };
-
-export const sortData = (data,sortBy,sortOrder) => {
-  return [];
-};
+// export const sortData = (data,sortBy,sortOrder) => {
+//   return [];
+// };
