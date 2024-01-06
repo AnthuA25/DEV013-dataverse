da856b8d2a4fb42db7900f41b61400a55de34a8b
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2024-01-06 15:25:17 -0400

    feat(main.js): implement alphabetical sorting (asc, desc) function and reset button

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { filterData,sortData } from './dataFunctions.js';
+import { filterData,sortData} from './dataFunctions.js';
 import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
@@ -6,5 +6,23 @@ import data from './data/dataset.js';
 // console.log(example, renderItems(data), data);
 
 const containerCard = document.querySelector("#root");
+const sortOrderSelect=document.getElementById('name'); 
+
+//reset button
+const resetbutton= document.querySelector("[type=\"reset\"]");
+resetbutton.addEventListener('click',()=>{
+  containerCard.innerHTML = renderItems(data);
+});
+
+//alfabetic order
+sortOrderSelect.addEventListener('change',()=>{
+  const sortOrder=sortOrderSelect.value;
+  const sortedData=sortData(data, 'name',sortOrder);
+  containerCard.innerHTML = renderItems(sortedData);
+
+});
+
+
+
 
 containerCard.innerHTML = renderItems(data);
