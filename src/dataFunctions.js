9c41432af6aebaf2202b2268b06122d40a1b8c07
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2024-01-06 15:25:53 -0400

    feat(dataFunctions.js): add alphabetical sorting (asc, desc) function

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,9 +1,30 @@
 // Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
 
-export const filterData = (data, filterBy,value) => {
+export const filterData = (data, filterBy, value) => {
   return 'example';
 };
 
-export const sortData = (data,sortBy,sortOrder) => {
-  return [];
+//data=data, sortBy=name, sorOrder=asc, desc
+export const sortData = (data, sortBy, sortOrder) => {
+  const sortedData = [...data]; //create copy array
+  console.log(sortedData);
+
+
+  sortedData.sort((a, b) => {
+    const nameA = a[sortBy].toLowerCase();
+    const nameB = b[sortBy].toLowerCase();
+
+    if (sortOrder === 'asc') {
+      return nameA.localeCompare(nameB);
+      //console.log(a.name.localeCompare(b.name))
+
+    } else if (sortOrder === 'desc') {
+      return nameB.localeCompare(nameA);
+      //console.log(b.name.localeCompare(a.name))
+    }
+
+  })
+
+
+  return sortedData;
 };
