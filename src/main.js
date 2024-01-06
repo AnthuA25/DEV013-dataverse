401ff72c7e8c2e0d1a29f53ed97d4c57324c041c
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2024-01-06 12:39:17 -0400

    feat(main.js): import data and display in HTML

--- src/main.js
+++ src/main.js
@@ -3,4 +3,8 @@ import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
 
+const cards=document.querySelector('#root');
+cards.innerHTML=renderItems(data);
+
+
 console.log(example, renderItems(data), data);
