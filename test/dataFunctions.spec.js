b10dd1c8b0ffda2453f226f56d7b775244276ff7
Aylin <aylin_santacruz@hotmail.com>
2024-01-10 13:56:06 -0500

    test:add test filterData

--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -1,42 +1,42 @@
-import { expect } from '@jest/globals';
-import { sortData } from '../src/dataFunctions.js';
-
+import { filterData, sortData } from '../src/dataFunctions.js';
 // import { data as fakeData } from './data.js';
-// 1. Fn existe
-it('sortData existe', ()=>{
-  expect(typeof sortData).toBe('function')
-})
-// 2. fn funciona (comportamiento deseado)
-it('', ()=>{
-  const TEST_SORT = [{name:"Bulsaur"},{name:'Abra'},{name:'Caterpie'}];
-  const sortBy = 'name';
-  const sortOrder = 'asc'
-  expect(sortData(TEST_SORT, sortBy, sortOrder )).toEqual([{"name": "Abra"}, {"name": "Bulsaur"}, {"name": "Caterpie"}])
-})
-// 3. Estresar al test
-it('', ()=>{
-  const TEST_SORT = [];
-  const sortBy = 'name';
-  const sortOrder = 'asc'
-  // expect(sortData(TEST_SORT, sortBy, sortOrder )).toBe(false)
-  expect(sortData(TEST_SORT, sortBy, sortOrder )).toBeFalsy()
-})
+
+describe('SORTDATA', () =>{
+  
+  // 1. Fn existe
+  it('SORTDATA EXISTS', ()=>{
+    expect(typeof sortData).toBe('function');
+  });
+  // 2. fn funciona (comportamiento deseado)
+  it('SORT ASCENDING', ()=>{
+    const TEST_SORT = [{name:"Bulsaur"},{name:'Abra'},{name:'Caterpie'}];
+    const sortBy = 'name';
+    const sortOrder = 'asc'
+    expect(sortData(TEST_SORT, sortBy, sortOrder )).toEqual([{"name": "Abra"}, {"name": "Bulsaur"}, {"name": "Caterpie"}]);
+  });
+  // 3. Estresar al test
+  it('DATA EMPTY', ()=>{
+    const TEST_SORT = [];
+    const sortBy = 'name';
+    const sortOrder = 'asc'
+    // expect(sortData(TEST_SORT, sortBy, sortOrder )).toBe(false)
+    expect(sortData(TEST_SORT, sortBy, sortOrder )).toBeFalsy();
+  });
+});
 
 /**  REVISAR AAA EN TESTING**/
+describe('FILTERDATA', () => {
 
-// console.log(fakeData);
-// const TEST_SORT = [{name:"Bulsaur"},{name:'Abra'},{name:'Caterpie'}];
+  it('FILTERDATA EXISTS', () => {
+    expect(typeof filterData).toBe('function');
+  });
 
-// describe('sortData', () => {
+  it('FILTERDATA FOR POKEMONS', () => {
+    const TEST_FILTER = [{name:"Bulsaur",type:{typeName:['Poison','Grass']}},{name:'Abra',type:{typeName:['Psychic']}},{name:'Ivysaur',type:{typeName:['Poison','Grass']}}]
+    const filterBy = 'typeName';
+    const value = 'Grass';
+    expect(filterData(TEST_FILTER,filterBy,value)).toEqual([{"name": "Bulsaur", "type": {"typeName": ["Poison", "Grass"]}}, {"name": "Ivysaur", "type": {"typeName": ["Poison", "Grass"]}}]);
+  });
 
-//   it.only('should return  `example`', () => {
-//     expect(sortData(TEST_SORT)).toEqual([{name:'Abra'},{name:'Bulbasaur'},{name:'Caterpie'}]);
-//   });
-// });
+});
 
-// describe('filterData', () => {
-
-//   it('returns `anotherExample`', () => {
-//     expect(anotherExample()).toBe('OMG');
-//   });
-// });
