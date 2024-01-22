699a9b0e835190c7c11626d5b52da32a3d17a426
Aylin <aylin_santacruz@hotmail.com>
2024-01-21 20:42:00 -0500

    Updates

---
+++ dataFunctions.js
@@ -0,0 +1,76 @@
+// Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
+
+//data=data, sortBy=name, sorOrder=asc, desc
+export const sortData = (data, sortBy, sortOrder) => {
+  if (data.length === 0) {
+    return false;
+  }
+  const sortedData = data; //create copy array
+  // console.log(sortedData);
+
+  sortedData.sort((a, b) => {
+    const nameA = a[sortBy].toLowerCase();
+    const nameB = b[sortBy].toLowerCase();
+
+    if (sortOrder === "asc") {
+      return nameA.localeCompare(nameB);
+      //console.log(a.name.localeCompare(b.name))
+    } else if (sortOrder === "desc") {
+      return nameB.localeCompare(nameA);
+      //console.log(b.name.localeCompare(a.name))
+    }
+  });
+
+  return sortedData;
+};
+
+export const filterData = (data, filterBy, value) => {
+  const result = data.filter((type) => type.type[filterBy].includes(value));
+  return result;
+};
+
+export const computeStats = (originalData) => {
+  // const result = data.filter(type => type.type.type_name.includes(value)).length;
+  // return result;
+  let names = [];
+  //const nroPokemons = [];
+  // eslint-disable-next-line no-undef
+  const uniqueType = new Set();
+  // for (let i = 0; i < originalData.length; i++) {
+  //   const types = originalData[i].type.typeName;
+  //   for (let j = 0; j < types.length; j++) {
+  //     uniqueType.add(types[j]);
+  //   }
+  // }
+  originalData.forEach((data) =>{
+    const types = data.type.typeName;
+    types.forEach((el) => {
+      uniqueType.add(el);
+    })
+  })
+  
+  names = Array.from(uniqueType);
+  
+  /* let sametypes = 0;
+  for (let i = 0; i < names.length; i++) {
+    sametypes = originalData.filter((type) =>
+      type.type.typeName.includes(names[i])
+    ).length;
+    // console.log(sametypes);
+    nroPokemons.push(sametypes);
+  }  */
+
+  //originalData.filter((pokemon) => pokemon.type.typeName.includes(typeName)).length
+  const nroPokemons = names.map((typeName) =>
+    originalData.reduce((accumulator, pokemon) => {
+      if (pokemon.type.typeName.includes(typeName)) {
+        accumulator += 1;
+      }
+      return parseInt(accumulator);
+    }, 0)
+  );
+  return {
+    names,nroPokemons
+  }
+
+};
---
+++ main.js
@@ -0,0 +1,128 @@
+import { filterData, sortData, computeStats } from "./dataFunctions.js";
+import { renderItems } from "./view.js";
+
+import data from "./data/dataset.js";
+//import Chart from 'chart.js/auto';
+
+const containerCard = document.querySelector("ul");
+const sortOrderSelect = document.getElementById("name");
+const filterType = document.getElementById("type");
+const form = document.getElementById("formFilters");
+const searchPokemons = document.querySelector("input[type='text']");
+const modal = document.getElementById("myBtn");
+const modalContent = document.querySelector(".modal-content");
+const close = document.querySelector(".fa-xmark");
+const stadistic = document.getElementById("myChart");
+//overlay
+const overlay = document.querySelector(".overlay");
+const resetbutton = document.querySelector('[type="reset"]');
+// const infoPokemons = document.querySelector("dl[itemprop='pokemons']")
+// console.log(infoPokemons)
+
+// Modal
+modal.addEventListener("click", () => {
+  modalContent.classList.toggle("modal-active");
+  overlay.classList.toggle("overlay-active");
+});
+close.addEventListener("click", () => {
+  modalContent.classList.toggle("modal-active");
+  overlay.classList.toggle("overlay-active");
+});
+
+// create copy
+const originalData = data;
+// current data
+let currentData = originalData;
+
+//
+const renderCurrentData = () => {
+  containerCard.innerHTML = renderItems(currentData);
+  // console.log(containerCard.querySelectorAll("dd[itemprop='image']"));
+};
+
+// boton pokemons search
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  currentData = [];
+  const value = searchPokemons.value;
+  const findPokemons = data.find((data) => data.name.toLowerCase() === value);
+  if (findPokemons) {
+    currentData.push(findPokemons);
+    renderCurrentData(currentData);
+    searchPokemons.value = "";
+  } else {
+    const container = document.createElement("section");
+    container.classList.add("container-modal");
+    overlay.classList.toggle("overlay-active");
+    container.innerHTML = `<i class="fa-solid fa-xmark"></i>
+                            <img src="./assets/pokemones/icono-cerrar.png" alt="Error"/>
+                            <p>Pokemon no encontrado</p>`;
+    container.querySelector(".fa-xmark").addEventListener("click", () => {
+      container.remove();
+      overlay.classList.toggle("overlay-active");
+    });
+    document.querySelector("body").appendChild(container);
+    searchPokemons.value = "";
+  }
+});
+
+// Resetear al estado original
+resetbutton.addEventListener("click", () => {
+  currentData = originalData;
+  containerCard.innerHTML = renderItems(currentData);
+});
+
+// alfabetic order
+sortOrderSelect.addEventListener("change", () => {
+  const sortOrder = sortOrderSelect.value;
+  currentData = sortData(currentData, "name", sortOrder);
+  renderCurrentData();
+});
+
+// Filter Type
+filterType.addEventListener("change", () => {
+  const selectedFilter = filterType.value;
+  currentData = filterData(originalData, "typeName", selectedFilter);
+  currentData = sortData(currentData, "name", sortOrderSelect.value);
+  renderCurrentData();
+});
+
+const updateChart = (names, nroPokemons) => {
+  // eslint-disable-next-line no-undef
+  new Chart(stadistic, {
+    type: "bar",
+    data: {
+      labels: names,
+      datasets: [
+        {
+          label: "# of Pokemons for type",
+          data: nroPokemons,
+          borderWidth: 1,
+          backgroundColor: "#9BD0F5",
+          font: {
+            size: 14,
+            weight: "bolder",
+          },
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+};
+
+const resultchart = computeStats(originalData);
+const names = resultchart.names;
+const nroPokemons = resultchart.nroPokemons;
+updateChart(names, nroPokemons);
+
+// info
+
+renderCurrentData();
+
+// console.log(pokemonsAverage(originalData))
